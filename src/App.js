b84d868079115c3677a4802b3d90640ffba02107
rival104 <navid.rvl@gmail.com>
2020-01-29 00:27:38 -0500

    added particle background to app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from "react-particles-js";
 import Navigation from './components/Navigation/Navigation';
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+const particlesOptions = {
+  "particles": {
+	        "number": {
+              "value": 50,
+              density: {
+                enable: true,
+                value_area: 600
+              } 
+          },
+          
+	        "size": {
+	            "value": 3
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        }
+	    }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions}/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
